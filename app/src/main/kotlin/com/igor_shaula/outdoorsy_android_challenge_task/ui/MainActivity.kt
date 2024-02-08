973e23cfdfbfa9e4c14e1bf68b343158eb4ad1da
igor.shaula <igor.shaula@gmail.com>
2024-02-08 22:52:58 +0200

    + ProgressBar as the starting UI, used data from the VM in UI

--- app/src/main/kotlin/com/igor_shaula/outdoorsy_android_challenge_task/ui/MainActivity.kt
+++ app/src/main/kotlin/com/igor_shaula/outdoorsy_android_challenge_task/ui/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.igor_shaula.outdoorsy_android_challenge_task.ui
 
 import android.os.Bundle
+import android.widget.ProgressBar
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import com.igor_shaula.outdoorsy_android_challenge_task.data.FakeDataSource.Companion.fakeVehiclesList
 import com.igor_shaula.outdoorsy_android_challenge_task.ui.elements.TheAppUI
 
 class MainActivity : ComponentActivity() {
@@ -14,12 +14,13 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // todo: add app toolbar for later search request
+        setContentView(ProgressBar(this))
     }
 
     override fun onStart() {
         super.onStart()
-        viewModel.vehiclesList.observe(this) {
-            setContent { TheAppUI(fakeVehiclesList) }
+        viewModel.vehiclesList.observe(this) { vehicles ->
+            setContent { TheAppUI(vehicles) }
         }
     }
 }
