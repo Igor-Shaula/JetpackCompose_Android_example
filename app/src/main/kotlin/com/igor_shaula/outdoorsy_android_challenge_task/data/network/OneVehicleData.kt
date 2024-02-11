a0ff0a883f69bba185c47f91d315c30c97620faf
igor.shaula <igor.shaula@gmail.com>
2024-02-11 02:31:33 +0200

    + OneVehicleData as main business-logic data container

---
+++ app/src/main/kotlin/com/igor_shaula/outdoorsy_android_challenge_task/data/network/OneVehicleData.kt
@@ -0,0 +1,7 @@
+package com.igor_shaula.outdoorsy_android_challenge_task.data.network
+
+data class OneVehicleData(
+    val name: String,
+    val imageId: String,
+    var imageUrl: String = ""
+)
