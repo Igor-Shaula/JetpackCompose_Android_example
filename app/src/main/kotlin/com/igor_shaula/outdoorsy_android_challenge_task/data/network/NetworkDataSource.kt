c84f04ae7024e945d857fb7843ed5f121a97054c
igor.shaula <igor.shaula@gmail.com>
2024-02-11 02:25:10 +0200

    cleared NetworkDataSource from all debug stuff

--- app/src/main/kotlin/com/igor_shaula/outdoorsy_android_challenge_task/data/network/NetworkDataSource.kt
+++ app/src/main/kotlin/com/igor_shaula/outdoorsy_android_challenge_task/data/network/NetworkDataSource.kt
@@ -1,5 +1,6 @@
 package com.igor_shaula.outdoorsy_android_challenge_task.data.network
 
+import com.igor_shaula.outdoorsy_android_challenge_task.data.entities.VehicleNetworkEntity
 import com.igor_shaula.outdoorsy_android_challenge_task.data.network.retrofit.VehicleRetrofitNetworkService
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -13,20 +14,6 @@ class NetworkDataSource {
             .build()
             .create(VehicleRetrofitNetworkService::class.java)
 
-//    private val testBeerService: TestBeerService = Retrofit.Builder()
-//        .baseUrl("https://api.punkapi.com/")
-//        .addConverterFactory(GsonConverterFactory.create())
-//        .build()
-//        .create(TestBeerService::class.java)
-
-    suspend fun launchSearchRequestFor(searchQuery: String) {
-        println("launchSearchRequestFor Retrofit: $searchQuery")
-
-//        val beers = testBeerService.getBeers() // it does work
-//        println("beers = $beers")
-
-        val vehicleNetworkEntity = vehicleNetworkService.getVehiclesList(searchQuery)
-        println("response: vehicleNetworkEntity = $vehicleNetworkEntity")
-
-    }
+    suspend fun launchSearchRequestFor(searchQuery: String): VehicleNetworkEntity =
+        vehicleNetworkService.getVehiclesList(searchQuery)
 }
