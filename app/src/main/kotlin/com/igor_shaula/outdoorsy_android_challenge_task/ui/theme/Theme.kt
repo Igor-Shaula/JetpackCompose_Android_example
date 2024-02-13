65d63c0f3a3876281ec75ea00df48a8c142fc55c
igor.shaula <igor.shaula@gmail.com>
2024-02-13 03:13:33 +0200

    + GivenColorScheme in Theme.kt

--- app/src/main/kotlin/com/igor_shaula/outdoorsy_android_challenge_task/ui/theme/Theme.kt
+++ app/src/main/kotlin/com/igor_shaula/outdoorsy_android_challenge_task/ui/theme/Theme.kt
@@ -3,37 +3,40 @@ package com.igor_shaula.outdoorsy_android_challenge_task.ui.theme
 import android.app.Activity
 import android.os.Build
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
+//private val DarkColorScheme = darkColorScheme(
+//    primary = Purple80,
+//    secondary = PurpleGrey80,
+//    tertiary = Pink80
+//)
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+//private val LightColorScheme = lightColorScheme(
+//    primary = Purple40,
+//    secondary = PurpleGrey40,
+//    tertiary = Pink40
+//)
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+private val GivenColorScheme = lightColorScheme(
+    primary = DarkGreen,
+    secondary = Green,
+    tertiary = Color.White,
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.DarkGray,
+    onSecondary = Color.DarkGray,
+    onTertiary = Color.DarkGray,
+    onBackground = Color.DarkGray,
+    onSurface = Color.DarkGray
 )
 
 @Composable
@@ -49,9 +52,9 @@ fun OutdoorsyAndroidChallengeTaskTheme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+//        darkTheme -> DarkColorScheme
+//        else -> LightColorScheme
+        else -> GivenColorScheme
     }
     val view = LocalView.current
     if (!view.isInEditMode) {
