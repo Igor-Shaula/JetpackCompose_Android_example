905ee980b0fb58080314bc759051dab1468cf2e7
igor.shaula <igor.shaula@gmail.com>
2024-02-13 03:12:28 +0200

    left only given-in-sample greenish colors in Color.kt

--- app/src/main/kotlin/com/igor_shaula/outdoorsy_android_challenge_task/ui/theme/Color.kt
+++ app/src/main/kotlin/com/igor_shaula/outdoorsy_android_challenge_task/ui/theme/Color.kt
@@ -2,10 +2,14 @@ package com.igor_shaula.outdoorsy_android_challenge_task.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+//val Purple80 = Color(0xFFD0BCFF)
+//val PurpleGrey80 = Color(0xFFCCC2DC)
+//val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+//val Purple40 = Color(0xFF6650a4)
+//val PurpleGrey40 = Color(0xFF625b71)
+//val Pink40 = Color(0xFF7D5260)
+
+// these two colors were taken from screen which was given as a reference in the task's repository
+val DarkGreen = Color(red = 53, green = 94, blue = 33)
+val Green = Color(red = 74, green = 134, blue = 29)
