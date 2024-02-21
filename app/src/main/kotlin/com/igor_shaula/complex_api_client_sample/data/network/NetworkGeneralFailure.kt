adbd680688e4599a1be6690b1620abd7bfc544f4
igor.shaula <igor.shaula@gmail.com>
2024-02-22 01:04:16 +0200

    expanded NetworkGeneralFailure with initialThrowable value and ABSENT_ERROR_CODE constant

--- app/src/main/kotlin/com/igor_shaula/complex_api_client_sample/data/network/NetworkGeneralFailure.kt
+++ app/src/main/kotlin/com/igor_shaula/complex_api_client_sample/data/network/NetworkGeneralFailure.kt
@@ -1,3 +1,11 @@
 package com.igor_shaula.complex_api_client_sample.data.network
 
-data class NetworkGeneralFailure(val errorCode: Int, val errorMessage: String?) : Throwable()
+data class NetworkGeneralFailure(
+    val errorCode: Int = ABSENT_ERROR_CODE,
+    val errorMessage: String?, // for now this one is decided to be the only mandatory field
+    val initialThrowable: Throwable? = null
+) : Throwable() {
+    companion object {
+        const val ABSENT_ERROR_CODE = -1
+    }
+}
