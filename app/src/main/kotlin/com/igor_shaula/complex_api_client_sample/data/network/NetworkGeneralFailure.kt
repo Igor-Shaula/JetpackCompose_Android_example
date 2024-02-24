891980f35ea3dfe4c875840198d360f44015d247
igor.shaula <igor.shaula@gmail.com>
2024-02-24 23:14:10 +0200

    moved .prepareExplanation() into NetworkGeneralFailure which is more natural

--- app/src/main/kotlin/com/igor_shaula/complex_api_client_sample/data/network/NetworkGeneralFailure.kt
+++ app/src/main/kotlin/com/igor_shaula/complex_api_client_sample/data/network/NetworkGeneralFailure.kt
@@ -6,8 +6,25 @@ data class NetworkGeneralFailure(
     val errorMessage: String = ABSENT_ERROR_MESSAGE,
     val initialThrowable: Throwable? = null
 ) : Throwable() {
+
     companion object {
         const val ABSENT_ERROR_CODE = -1
         const val ABSENT_ERROR_MESSAGE = ""
+    }
+
+    internal fun prepareExplanation(): String {
+        val explanation = StringBuilder()
+        if (this.initialThrowable != null) {
+            explanation.append(initialThrowable.javaClass.name + "\n" + initialThrowable.message)
+            return explanation.toString()
+        }
+        explanation.append("NetworkGeneralFailure:\n")
+        if (this.errorCode != NetworkGeneralFailure.ABSENT_ERROR_CODE) {
+            explanation.append("errorCode: $errorCode\n")
+        }
+        if (this.errorMessage != NetworkGeneralFailure.ABSENT_ERROR_MESSAGE) {
+            explanation.append("errorMessage: $errorMessage\n")
+        }
+        return explanation.toString()
     }
 }
--- app/src/main/kotlin/com/igor_shaula/complex_api_client_sample/domain/GenericErrorForUI.kt
+++ app/src/main/kotlin/com/igor_shaula/complex_api_client_sample/domain/GenericErrorForUI.kt
@@ -1,21 +1,3 @@
 package com.igor_shaula.complex_api_client_sample.domain
 
-import com.igor_shaula.complex_api_client_sample.data.network.NetworkGeneralFailure
-
 data class GenericErrorForUI(val explanation: String? = null) : Throwable()
-
-internal fun NetworkGeneralFailure.prepareExplanation(): String {
-    val explanation = StringBuilder()
-    if (this.initialThrowable != null) {
-        explanation.append(initialThrowable.javaClass.name + "\n" + initialThrowable.message)
-        return explanation.toString()
-    }
-    explanation.append("NetworkGeneralFailure:\n")
-    if (this.errorCode != NetworkGeneralFailure.ABSENT_ERROR_CODE) {
-        explanation.append("errorCode: $errorCode\n")
-    }
-    if (this.errorMessage != NetworkGeneralFailure.ABSENT_ERROR_MESSAGE) {
-        explanation.append("errorMessage: $errorMessage\n")
-    }
-    return explanation.toString()
-}
