b9eaa33b605e3541956fb8212120416e222dd016
igor.shaula <igor.shaula@gmail.com>
2024-02-25 03:18:07 +0200

    updated all Hilt dependencies to 2.50 to fix builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,8 +65,8 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("io.coil-kt:coil-compose:2.5.0")
 
-    implementation("com.google.dagger:hilt-android:2.44")
-    kapt("com.google.dagger:hilt-android-compiler:2.44")
+    implementation("com.google.dagger:hilt-android:2.50")
+    kapt("com.google.dagger:hilt-android-compiler:2.50")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
