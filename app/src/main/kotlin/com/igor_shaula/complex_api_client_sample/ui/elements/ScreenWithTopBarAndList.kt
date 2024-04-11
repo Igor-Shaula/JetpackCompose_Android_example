e69ccc8bd1faa11b3fe0f6aed1c66478c94cc2bf
Igor Shaula <igor.shaula@gmail.com>
2024-04-11 03:24:10 +0300

    moved TopBarUI() into separate composable

--- app/src/main/kotlin/com/igor_shaula/complex_api_client_sample/ui/elements/ScreenWithTopBarAndList.kt
+++ app/src/main/kotlin/com/igor_shaula/complex_api_client_sample/ui/elements/ScreenWithTopBarAndList.kt
@@ -1,20 +1,12 @@
 package com.igor_shaula.complex_api_client_sample.ui.elements
 
-import androidx.compose.foundation.layout.height
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.igor_shaula.complex_api_client_sample.R
 import com.igor_shaula.complex_api_client_sample.ui.MainViewModel
-import com.igor_shaula.complex_api_client_sample.ui.theme.APP_BAR_HEIGHT
-import com.igor_shaula.complex_api_client_sample.ui.theme.DEFAULT_ELEVATION
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ScreenWithTopBarAndList(hideKeyboard: () -> Unit) {
 
@@ -23,16 +15,9 @@ fun ScreenWithTopBarAndList(hideKeyboard: () -> Unit) {
 
     Scaffold(
         topBar = {
-            TopAppBar(
-                title = {
-                    CustomizedSearchBarAlternative(
-                        viewModel.searchQueryForUI
-                    ) { query, isForced -> viewModel.updateSearchRequest(query, isForced) }
-                },
-                modifier = Modifier
-                    .height(APP_BAR_HEIGHT)
-                    .shadow(elevation = DEFAULT_ELEVATION)
-            )
+            TopBarUI(viewModel.searchQueryForUI) { query, isForced ->
+                viewModel.updateSearchRequest(query, isForced)
+            }
         }
     ) { innerPadding -> // use this thing somehow - because warning emerges if it's not used
         println("innerPadding = $innerPadding")
---
+++ app/src/main/kotlin/com/igor_shaula/complex_api_client_sample/ui/elements/TopBarUI.kt
@@ -0,0 +1,23 @@
+package com.igor_shaula.complex_api_client_sample.ui.elements
+
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import com.igor_shaula.complex_api_client_sample.ui.theme.APP_BAR_HEIGHT
+import com.igor_shaula.complex_api_client_sample.ui.theme.DEFAULT_ELEVATION
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBarUI(searchQuery: String, onSearchQueryUpdate: (String, Boolean) -> Unit) {
+    TopAppBar(
+        title = {
+            CustomizedSearchBarAlternative(searchQuery, onSearchQueryUpdate)
+        },
+        modifier = Modifier
+            .height(APP_BAR_HEIGHT)
+            .shadow(elevation = DEFAULT_ELEVATION)
+    )
+}
