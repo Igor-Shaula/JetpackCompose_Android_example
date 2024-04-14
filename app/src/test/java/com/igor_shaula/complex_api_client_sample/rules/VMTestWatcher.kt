7aad495f4cfa780c38385b235648fd10ce4b5f9a
Igor Shaula <igor.shaula@gmail.com>
2024-04-15 01:33:43 +0300

    optimized VMTestWatcher a bit

--- app/src/test/java/com/igor_shaula/complex_api_client_sample/rules/VMTestWatcher.kt
+++ app/src/test/java/com/igor_shaula/complex_api_client_sample/rules/VMTestWatcher.kt
@@ -2,28 +2,27 @@ package com.igor_shaula.complex_api_client_sample.rules
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class VMTestWatcher(
-    private val testDispatcher: TestDispatcher = StandardTestDispatcher()
+    private val testDispatcher: TestDispatcher = UnconfinedTestDispatcher() // exactly this for VM
 ) : TestWatcher() {
 
-    @OptIn(ExperimentalCoroutinesApi::class)
     override fun starting(description: Description?) {
         super.starting(description)
-        println("starting: description = $description")
+//        println("starting: description = $description")
         Dispatchers.setMain(testDispatcher)
     }
 
-    @OptIn(ExperimentalCoroutinesApi::class)
     override fun finished(description: Description) {
         super.finished(description)
-        println("finished: description = $description")
+//        println("finished: description = $description")
         Dispatchers.resetMain()
     }
 }
