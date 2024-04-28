cc24397554c5eefdcf73f8c82111d62e8555cc36
Igor Shaula <igor.shaula@gmail.com>
2024-04-29 01:46:30 +0300

    adjusted Type.kt with all settings for all texts

--- app/src/main/kotlin/com/igor_shaula/complex_api_client_sample/ui/theme/Type.kt
+++ app/src/main/kotlin/com/igor_shaula/complex_api_client_sample/ui/theme/Type.kt
@@ -2,35 +2,51 @@ package com.igor_shaula.complex_api_client_sample.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 
 // Set of Material 3 typography styles to start with
 val typography = Typography(
+
+    // used in ExplanationScreen
     headlineSmall = TextStyle(
         fontWeight = FontWeight.SemiBold,
-        fontSize = 24.sp,
+        fontSize = BIG_FONT_SIZE,
         lineHeight = 32.sp,
-        letterSpacing = 0.sp
+        letterSpacing = 0.sp,
+        fontStyle = FontStyle.Normal,
+        fontFamily = FontFamily.SansSerif,
+        textAlign = TextAlign.Center
     ),
+
     titleLarge = TextStyle(
         fontWeight = FontWeight.Normal,
         fontSize = 18.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
+
+    // used in TopAppBar for Search field content & placeholder
     bodyLarge = TextStyle(
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
+        fontSize = DEFAULT_FONT_SIZE,
         lineHeight = 24.sp,
-        letterSpacing = 0.15.sp
+        letterSpacing = 0.15.sp,
+        fontStyle = FontStyle.Normal,
+        fontFamily = FontFamily.SansSerif,
+        textAlign = TextAlign.Start
     ),
+
     bodyMedium = TextStyle(
         fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.25.sp
     ),
+
     labelMedium = TextStyle(
         fontWeight = FontWeight.SemiBold,
         fontSize = 12.sp,
