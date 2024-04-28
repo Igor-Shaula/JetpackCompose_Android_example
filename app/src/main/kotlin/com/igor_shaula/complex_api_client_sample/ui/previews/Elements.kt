c27e0b1f29aae7fa87226e5b82e9d3a8239e741d
Igor Shaula <igor.shaula@gmail.com>
2024-04-29 02:40:47 +0300

    + TheAppTopBarPreview()

--- app/src/main/kotlin/com/igor_shaula/complex_api_client_sample/ui/previews/Elements.kt
+++ app/src/main/kotlin/com/igor_shaula/complex_api_client_sample/ui/previews/Elements.kt
@@ -5,8 +5,19 @@ import androidx.compose.ui.res.stringResource
 import com.igor_shaula.complex_api_client_sample.R
 import com.igor_shaula.complex_api_client_sample.ui.elements.CustomizedSearchBar
 import com.igor_shaula.complex_api_client_sample.ui.elements.CustomizedSearchBarAlternative
+import com.igor_shaula.complex_api_client_sample.ui.elements.TheAppTopBar
 import com.igor_shaula.complex_api_client_sample.ui.elements.TheUiCard
 import com.igor_shaula.complex_api_client_sample.ui.models.TheUiModel
+
+@Previews
+@Composable
+fun TheAppTopBarPreview() {
+    ThemeWithSurfaceElement {
+        TheAppTopBar(
+            searchQuery = stringResource(id = R.string.searchFieldHint),
+        ) { _, _ -> }
+    }
+}
 
 @Previews
 @Composable
