0be64103f7232c4d25fd2c04880349fe5a8764c4
Igor Shaula <igor.shaula@gmail.com>
2024-04-29 01:49:05 +0300

    used text style in ExplanationScreen()

--- app/src/main/kotlin/com/igor_shaula/complex_api_client_sample/ui/elements/ExplanationScreen.kt
+++ app/src/main/kotlin/com/igor_shaula/complex_api_client_sample/ui/elements/ExplanationScreen.kt
@@ -2,25 +2,17 @@ package com.igor_shaula.complex_api_client_sample.ui.elements
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
-import com.igor_shaula.complex_api_client_sample.ui.theme.BIG_FONT_SIZE
 import com.igor_shaula.complex_api_client_sample.ui.theme.DEFAULT_PADDING
 
 @Composable
 fun ExplanationScreen(paddingTop: Dp, theText: String) = Text(
     text = theText,
-    textAlign = TextAlign.Center,
-    fontFamily = FontFamily.Cursive,
-    fontWeight = FontWeight.Bold,
-    fontStyle = FontStyle.Normal,
-    fontSize = BIG_FONT_SIZE,
+    style = MaterialTheme.typography.headlineSmall,
     modifier = Modifier
         .fillMaxSize()
         .padding(
