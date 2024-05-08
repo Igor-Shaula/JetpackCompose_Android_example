e2ee0b03460448b3ba21761e5a7a0915e6a25224
Igor Shaula <igor.shaula@gmail.com>
2024-05-08 08:40:31 +0300

    replaced getChosenApi() & selectedApi with activeApiFlow

--- app/src/main/kotlin/com/igor_shaula/complex_api_client_sample/data/repositories/SettingsRepository.kt
+++ app/src/main/kotlin/com/igor_shaula/complex_api_client_sample/data/repositories/SettingsRepository.kt
@@ -1,5 +1,8 @@
 package com.igor_shaula.complex_api_client_sample.data.repositories
 
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 const val OUTDOORSY_UI_NAME = "Outdoorsy"
@@ -16,18 +19,18 @@ enum class ActiveApi(val uiName: String) {
 
 interface SettingsRepository {
 
-    fun setChosenApi(newApi: ActiveApi)
+    val activeApiFlow: StateFlow<ActiveApi>
 
-    fun getChosenApi(): ActiveApi
+    fun setActiveApi(newApi: ActiveApi)
 }
 
 class SettingsRepositoryImpl @Inject constructor() : SettingsRepository {
 
-    private var selectedApi: ActiveApi = ActiveApi.OUTDOORSY // this API will be selected by default
+    private val _activeApi = MutableStateFlow(ActiveApi.OUTDOORSY)
+    override val activeApiFlow: StateFlow<ActiveApi> = _activeApi.asStateFlow()
 
-    override fun setChosenApi(newApi: ActiveApi) {
-        selectedApi = newApi
+    override fun setActiveApi(newApi: ActiveApi) {
+        _activeApi.value = newApi
+//        _activeApi.emit(newApi) // the same as previous but requires to be inside a coroutine scope
     }
-
-    override fun getChosenApi(): ActiveApi = selectedApi
 }
