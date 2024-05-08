8df4dec9a2b8b17b87b207925c3ad69523c9b2da
Igor Shaula <igor.shaula@gmail.com>
2024-05-08 08:46:52 +0300

    + setupForChangingActiveApi() & DrawerViewModel

--- app/src/main/kotlin/com/igor_shaula/complex_api_client_sample/ui/view_models/DrawerViewModel.kt
+++ app/src/main/kotlin/com/igor_shaula/complex_api_client_sample/ui/view_models/DrawerViewModel.kt
@@ -4,9 +4,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.igor_shaula.complex_api_client_sample.data.repositories.ActiveApi
 import com.igor_shaula.complex_api_client_sample.data.repositories.SettingsRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -17,8 +19,20 @@ class DrawerViewModel @Inject constructor(
     var uiState: DrawerUiState by mutableStateOf(DrawerUiState(ActiveApi.OUTDOORSY))
         private set
 
+    init {
+        setupForChangingActiveApi()
+    }
+
     fun onActiveApiChange(newApi: String) {
-        settingsRepository.setChosenApi(ActiveApi.fromName(newApi))
-        uiState = DrawerUiState(settingsRepository.getChosenApi())
+        settingsRepository.setActiveApi(ActiveApi.fromName(newApi))
+//        uiState = DrawerUiState(settingsRepository.getChosenApi()) // -> setupForChangingActiveApi()
+    }
+
+    private fun setupForChangingActiveApi() {
+        viewModelScope.launch {
+            settingsRepository.activeApiFlow.collect { activeApi ->
+                uiState = DrawerUiState(activeApi)
+            }
+        }
     }
 }
