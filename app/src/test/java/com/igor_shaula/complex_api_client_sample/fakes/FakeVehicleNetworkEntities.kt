1a55ade3059d8a777eac71460548ca878681e334
Igor Shaula <igor.shaula@gmail.com>
2024-07-16 23:17:51 +0300

    + data_sources package in tests & placed relevant file there

--- app/src/test/java/com/igor_shaula/complex_api_client_sample/fakes/FakeVehicleNetworkEntities.kt
+++ app/src/test/java/com/igor_shaula/complex_api_client_sample/fakes/FakeVehicleNetworkEntities.kt
@@ -1,5 +1,7 @@
 package com.igor_shaula.complex_api_client_sample.fakes
 
+import com.igor_shaula.complex_api_client_sample.data_sources.FakeDataSource
+
 val fakeVehicleNetworkEntityWithFullData =
     FakeDataSource(mockResponseWithFullData).processMockResponse().getOrNull()
 
