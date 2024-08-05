0c052cf1fa398118c19623ebbd1e7920ce48a958
Igor Shaula <igor.shaula@gmail.com>
2024-08-05 13:22:10 +0300

    + Mockito into the project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,6 +82,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.0")
 //    testImplementation("androidx.arch.core:core-testing:2.2.0")
+    testImplementation("org.mockito:mockito-core:5.2.1")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.2.1")
 
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
